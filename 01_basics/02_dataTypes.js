c8ed3b954f773874db1a07ef9ca533719c1bb951
vis0912 <163502985+vis0912@users.noreply.github.com>
2024-03-30 07:31:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all js codes as newer version codes
+
+// alert(3+3) //we are using node js, not browser
+
+console.log(3
+    +
+    3)  //code readability should be high
+console.log("Vishal")
+
+
+let name="Vishal"
+let age=20
+let isLoggedIn= false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  diff from null
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);      //object
+
+
+
+
+
+
+
+
