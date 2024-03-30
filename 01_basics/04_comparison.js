3ffaeca3b516d7c99bec4c8d9dea803e3ff54e56
vis0912 <163502985+vis0912@users.noreply.github.com>
2024-03-30 08:29:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,34 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);   //these 6 type of comparisons create confusion and are to be avoided
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// comparison(>,>=) and equality check(==) are two different things
+
+console.log("2"===2);  //strict check ===
+
+
+
+
+
+
+
+
+
+
+
+
+
+
