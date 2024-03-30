d32b070a74c717904dfacc8ce99a9ecc77828209
vis0912 <163502985+vis0912@users.noreply.github.com>
2024-03-30 08:51:51 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')    // Symbol is for creating uniques
+const anotherId=Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber= 347262637283837263637373272n   //add n at last
+
+// Reference (Non Primitive)
+// Array, Objects, Functions
+
+const heros=["Ironman","Captain","Thor"];
+
+let myObj= {
+    name:"vishal",
+    age:22
+}
+
+const myFunction= function(){
+    console.log("Hello World");
+}           
+
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
